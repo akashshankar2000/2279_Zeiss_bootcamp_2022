103854e85b9e2d7461029e3cfa444e63c6840cd1
akashshankar2000 <110802496+akashshankar2000@users.noreply.github.com>
2022-09-08 15:29:10 +0530

    update after review

--- assigments/runtime_polymorphism.cs
+++ assigments/runtime_polymorphism.cs
@@ -1,1 +1,74 @@
+namespace ConsoleApp16
+{
 
+    public class TataCar
+    {
+        
+	IEngineType _engineType;
+	//Depending on abstraction of car, Constructor injection
+	public TataCar(IEngineType engine)
+	{
+		this._engineType = engine;
+	}
+        //RevtronEngine _engine=new RevtronEngine(); //composition
+        public void Ignite() {
+        	_engine.Start();
+        }
+        public void Halt() {
+            _engine.Stop();
+        }
+    }
+
+interface IEngine
+{
+	void Start() { }
+
+        void Stop() { }
+}
+
+public class RevtronEngine : IEngine
+{
+	public void Start()
+	{
+		Console.WriteLine("Revtron is ignited");
+	}
+	public void Stop()
+	{
+	}
+}
+public class Vericore: IEngine
+{
+	public void Start()
+	{
+		Console.WriteLine("Vericore is ignited");
+	}
+	public void Stop()
+	{
+	}
+}
+public class Kryotech : IEngineType
+{
+	public void Start()
+	{
+		Console.WriteLine("Kryotech is ignited");
+	}
+	public void Stop()
+	{
+	}
+}
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+	    IEngineType _engine = new RevtronEngine()
+            TataCar _nexon = new TataCar(_engine);
+            _nexon.Ignite();
+       
+            _engine = new KryotechEngine();
+            TataCar _harrier = new TataCar();
+	    _harrier.Ignite();
+
+            TataCar _hexa = new TataCar();
+        }
+    }
